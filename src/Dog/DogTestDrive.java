46ec6641627ca84b144e83fc24d663e0437960ca
Axel_ST <Kozlov.axe@gmail.com>
2022-02-08 15:20:30 +0700

    Update Dog, DogTestDrive class files.

--- src/Dog/DogTestDrive.java
+++ src/Dog/DogTestDrive.java
@@ -7,4 +7,16 @@
 package Dog;
 
 public class DogTestDrive {
+    public static void main(String[] args) {
+        Dog one = new Dog();
+        Dog two = new Dog();
+        Dog three = new Dog();
+        one.size = 70;
+        two.size = 8;
+        three.size = 35;
+        
+        one.bark();
+        two.bark();
+        three.bark();
+    }
 }
