6a5adfdf0b7be37cc40c28fc6d7bf4917f54433a
Axel_ST <Kozlov.axe@gmail.com>
2022-03-10 11:41:25 +0700

    Update Clock, ClockTestDrive class files.

--- src/beTheCompiler/Clock.java
+++ src/beTheCompiler/Clock.java
@@ -7,4 +7,11 @@
 package beTheCompiler;
 
 public class Clock {
+    private String time;
+    public void setTime(String t) {
+        time = t;
+    }
+    public String getTime() {
+        return time;
+    }
 }
