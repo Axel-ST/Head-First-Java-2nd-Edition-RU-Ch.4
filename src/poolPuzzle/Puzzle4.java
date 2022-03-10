b04328298547b69c61027eb6f1bde5a09fbc70fd
Axel_ST <Kozlov.axe@gmail.com>
2022-03-10 14:59:59 +0700

    Update Puzzle4 class file. Puzzle4 class is updated.

--- src/poolPuzzle/Puzzle4.java
+++ src/poolPuzzle/Puzzle4.java
@@ -6,12 +6,31 @@
 package poolPuzzle;
 
 public class Puzzle4 {
-
+    public static void main(String[] args) {
+        Puzzle4b [] obs = new Puzzle4b[6];
+        int y = 1;
+        int x = 0;
+        int result = 0;
+        
+        while (x<6) {
+            obs[x] = new Puzzle4b();
+            obs[x].ivar = y;
+            y *= 10;
+            x++;
+        }
+        x = 6;
+        
+        while (x > 0) {
+            x--;
+            result += obs[x].doStuff(x);
+        }
+        System.out.println("result " + result);
+    }
 }
 
 class Puzzle4b{
     int ivar;
-    private int doStuff(int factor) {
+    public int doStuff(int factor) {
         if (ivar > 100) {
             return ivar * factor;
         } else {
