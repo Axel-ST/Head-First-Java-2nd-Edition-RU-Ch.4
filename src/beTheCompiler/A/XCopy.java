6546f9683d7604b6ceb746bfd2acb910f3791f1c
Axel_ST <Kozlov.axe@gmail.com>
2022-03-10 11:36:16 +0700

    Cpeate XCopy class file.

---
+++ src/beTheCompiler/A/XCopy.java
@@ -0,0 +1,21 @@
+/*
+ * Created by Axel_ST on 10.03.2022
+ *
+ * Head First Java, 2nd Edition (RU) p. 118
+ */
+
+package beTheCompiler.A;
+
+public class XCopy {
+    public static void main(String[] args) {
+        int orig = 42;
+        XCopy x = new XCopy();
+        int y = x.go(orig);
+        System.out.println(orig + " " + y);
+    }
+    
+    int go(int arg) {
+        arg *= 2;
+        return arg;
+    }
+}
