15e724c522468a22abd38bb3fa704fef0aca6420
Axel_ST <Kozlov.axe@gmail.com>
2022-03-10 14:29:32 +0700

    Update Mix4 class file.

--- src/mixedMessages/Mix4.java
+++ src/mixedMessages/Mix4.java
@@ -7,4 +7,34 @@
 package mixedMessages;
 
 public class Mix4 {
+    int counter = 0;
+    
+    public static void main(String[] args) {
+        int count = 0;
+        Mix4[] m4a = new Mix4[20];
+        int x = 0;
+        
+        while(x < 19) {
+            m4a[x] = new Mix4();
+            m4a[x].counter += 1;
+            count += 1;
+            count += m4a[x].maybeNew(x);
+            x += 1;
+        }
+        System.out.println(count + " " + m4a[1].counter);
+    }
+    /* Results:
+    * 9 & 5 -> 14 & 1
+    * 20 & 5 -> 25 & 1
+    * 7 & 7 -> 14 & 1
+    * 19 & 1 -> 20 & 1
+    */
+    public int maybeNew(int index) {
+        if (index < 1) {
+            Mix4 m4 = new Mix4();
+            m4.counter += 1;
+            return 1;
+        }
+        return 0;
+    }
 }
