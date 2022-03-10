9a81ec2f2951c482c28ef2c4c07dadf1bdec65c7
Axel_ST <Kozlov.axe@gmail.com>
2022-03-10 14:56:07 +0700

    Update Puzzle4 class file. Puzzle4b class is updated.

--- src/poolPuzzle/Puzzle4.java
+++ src/poolPuzzle/Puzzle4.java
@@ -6,8 +6,16 @@
 package poolPuzzle;
 
 public class Puzzle4 {
+
 }
 
 class Puzzle4b{
-
+    int ivar;
+    private int doStuff(int factor) {
+        if (ivar > 100) {
+            return ivar * factor;
+        } else {
+            return ivar * (5 - factor);
+        }
+    }
 }
